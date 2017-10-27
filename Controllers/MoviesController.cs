b7ef5a453bb948f233771e3b217e0662ceca9934
fcatarcione91 <fcatarcione91@gmail.com>
2017-10-27 02:02:14 -0400

    Initial commit

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie() { Id = 1, Name = "Shrek"},
+                new Movie() { Id = 2, Name = "Batman" }
+            };
+        }
+    }
+}
