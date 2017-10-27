5de47804eb5f9ed870300147c95c63197a74a45f
fcatarcione91 <fcatarcione91@gmail.com>
2017-10-27 12:13:09 -0400

    Added more properties to Movie model. Created Genre Model. Added migrations which created Movie database and Genre database.

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -10,21 +10,24 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        //private ApplicationDbContext _context;
+
+        //public MoviesController()
+        //{
+        //    _context = new ApplicationDbContext();
+        //}
+
+        //protected override void Dispose(bool disposing)
+        //{
+        //    _context.Dispose();
+        //}
+
         // GET: Movies
         public ActionResult Index()
         {
-            var movies = GetMovies();
+            //var movies = _context;
 
-            return View(movies);
-        }
-
-        private IEnumerable<Movie> GetMovies()
-        {
-            return new List<Movie>
-            {
-                new Movie() { Id = 1, Name = "Shrek"},
-                new Movie() { Id = 2, Name = "Batman" }
-            };
+            return View();
         }
     }
 }
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+using System.ComponentModel.DataAnnotations;
+
 namespace Vidly.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+        [Required]
+        public Genre Genre { get; set; }
+        public byte GenreId { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int NumberInStock { get; set; }
     }
 }
