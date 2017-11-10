c1a7f397b90de1eca1770982fc378e92577ea309
fcatarcione91 <fcatarcione91@gmail.com>
2017-11-09 22:51:16 -0500

    Updated WebApiConfig to configure my Web API to return json object using camel notation

--- App_Start/WebApiConfig.cs
+++ App_Start/WebApiConfig.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+
 namespace Vidly
 {
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
         {
+            //Configure Web API to return json object using camel notation:
+            var settings = config.Formatters.JsonFormatter.SerializerSettings;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            settings.Formatting = Formatting.Indented;
+
+
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
