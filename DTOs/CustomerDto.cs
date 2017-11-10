5deecdec0a720a08244dc6ef1b914fc17fb1e13d
fcatarcione91 <fcatarcione91@gmail.com>
2017-11-09 22:19:46 -0500

    Created a Web API Controller. Updated Global.asax.cs file to configure our web API. Created CustomerDTO class with most customer properties copied into it,

---
+++ DTOs/CustomerDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Vidly.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.DTOs
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)] 
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        public byte MembershipTypeId { get; set; } 
+
+        [Min18YearsIfAMember] 
+        public DateTime? Birthdate { get; set; }
+    }
+}
--- Global.asax.cs
+++ Global.asax.cs
@@ -6,12 +6,15 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
+using System.Web.Http;
+
 namespace Vidly
 {
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
