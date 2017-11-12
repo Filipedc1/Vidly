dfdc5524ba1d5fdab04b9382b64dbed65eceafed
fcatarcione91 <fcatarcione91@gmail.com>
2017-11-12 14:28:34 -0500

    Updated FilterConfig file to add Authorization attribute globally to restrict anonymous users from view app content. Updated HomeController by adding AllowAnonymous attribute to allow anonymous users to view the home page without having to login.

--- App_Start/FilterConfig.cs
+++ App_Start/FilterConfig.cs
@@ -1,6 +1,8 @@
 ﻿using System.Web;
 using System.Web.Mvc;
 
+//you can register global filters here.
+//e.g. HandleErrorAttribute is a default global filter. This filter redirects the user to an error page when an action throws an exception
 namespace Vidly
 {
     public class FilterConfig
@@ -8,6 +10,7 @@ namespace Vidly
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AuthorizeAttribute());
         }
     }
 }
