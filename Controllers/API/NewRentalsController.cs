d80e9dcc062e2e373349782277620dba0896e15b
fcatarcione91 <fcatarcione91@gmail.com>
2017-11-16 23:08:25 -0500

    Adding the ability to record new rentals as a feature. Created a NewRentals API Controller and a NewRentalDto class

---
+++ Controllers/API/NewRentalsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using Vidly.DTOs;
+
+namespace Vidly.Controllers.API
+{
+    public class NewRentalsController : ApiController
+    {
+        //HttpPost used because we will be creating new objects and this complies with RESTful conventions
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
