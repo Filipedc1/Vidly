4acaabf234b0a00e12927680bd71beab978de25d
fcatarcione91 <fcatarcione91@gmail.com>
2017-11-16 23:46:32 -0500

    Added logic to the CreateNewRentals action in the NewRentals API Controller.

--- Controllers/API/NewRentalsController.cs
+++ Controllers/API/NewRentalsController.cs
@@ -6,16 +6,42 @@ using System.Net.Http;
 using System.Web.Http;
 
 using Vidly.DTOs;
+using Vidly.Models;
 
 namespace Vidly.Controllers.API
 {
     public class NewRentalsController : ApiController
     {
+        private ApplicationDbContext _context;
+
+        public NewRentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         //HttpPost used because we will be creating new objects and this complies with RESTful conventions
         [HttpPost]
         public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
         {
-            throw new NotImplementedException();
+            var customer = _context.Customers.Single(c => c.Id == newRental.CustomerId);
+
+            var movies = _context.Movies.Where(m => newRental.MovieIds.Contains(m.Id));
+
+            foreach (var movie in movies)
+            {
+                var rental = new Rental()
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Now
+                };
+
+                _context.Rentals.Add(rental);
+            }
+
+            _context.SaveChanges();
+
+            return Ok();
         }
     }
 }
