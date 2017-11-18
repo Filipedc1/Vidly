888f850a3971bcd3fa5a31ffb0cacb8c9271d7fd
fcatarcione91 <fcatarcione91@gmail.com>
2017-11-18 17:36:42 -0500

    Added application settings. Updated web.config and startup.Auth files

--- App_Start/Startup.Auth.cs
+++ App_Start/Startup.Auth.cs
@@ -6,6 +6,7 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
 using Vidly.Models;
+using System.Configuration;
 
 namespace Vidly
 {
@@ -55,8 +56,8 @@ namespace Vidly
             //   consumerSecret: "");
 
             app.UseFacebookAuthentication(
-               appId: "1520224024680795",
-               appSecret: "35c05002c67113a325b23b128f544dec");
+               appId: ConfigurationManager.AppSettings["FacebookAppId"],
+               appSecret: ConfigurationManager.AppSettings["FacebookAppSecret"]);
 
             //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             //{
